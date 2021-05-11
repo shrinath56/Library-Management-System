5772d426533285340a5844b6441fa4be8250cebb
shrinath56 <46258187+shrinath56@users.noreply.github.com>
2021-05-11 19:19:56 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container ">
+            <div class="row ">
+                <div class="col-md-12 text-primary">
+                   &copy; 2019 Online Library Management System |<a href="" target="_blank"  > Designed by : Dominators</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
